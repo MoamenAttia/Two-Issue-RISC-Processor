37756c123f86ef64d1ba62cb57dbafcc85524d0b
Ahmed Maher <demha.ahmed@yahoo.com>
2019-04-27 18:47:44 +0200

    fu arch

--- VHDL/forwarding_unit.vhd
+++ VHDL/forwarding_unit.vhd
@@ -20,11 +20,11 @@ entity forwarding_unit is
 			id_ex_registerRs_2 	:in  STD_LOGIC_VECTOR(3 downto 0);
 			id_ex_registerRt_2 	:in  STD_LOGIC_VECTOR(3 downto 0);
 				
-			forward_a_1 		:out STD_LOGIC_VECTOR(2 downto 0);
-			forward_b_1 		:out STD_LOGIC_VECTOR(2 downto 0);
+			forward_a_1 		:out STD_LOGIC_VECTOR(2 downto 0); -- rsrc
+			forward_b_1 		:out STD_LOGIC_VECTOR(2 downto 0); -- rdst
 
-			forward_a_2 		:out STD_LOGIC_VECTOR(2 downto 0);
-			forward_b_2 		:out STD_LOGIC_VECTOR(2 downto 0)		
+			forward_a_2 		:out STD_LOGIC_VECTOR(2 downto 0); --rsrc
+			forward_b_2 		:out STD_LOGIC_VECTOR(2 downto 0)  --rdst	
 	);
 end forwarding_unit;
 
@@ -40,20 +40,24 @@ begin
 		
 		-- first operand in first instruction
 		if ((ex_mem_regWrite_1 = '1') -- EX HAZARD
+			and (ex_mem_registerRd_1 /= "0000")
 			and (ex_mem_registerRd_1 = id_ex_registerRs_1)) then
 				forward_a_1 <= "001"; 
 
 		elsif((ex_mem_regWrite_2 = '1')
+			 and (ex_mem_registerRd_2 /= "0000")
 			 and (ex_mem_registerRd_2 = id_ex_registerRs_1)) then
 				forward_a_1 <= "010";
 				
 		elsif ((mem_wb_regWrite_1 = '1') -- MEM HAZARD
+			and (ex_mem_registerRd_1 /= "0000")
 			and not(ex_mem_regWrite_1 = '1'
 				and (ex_mem_registerRd_1 = id_ex_registerRs_1))
 			and (mem_wb_registerRd_1 = id_ex_registerRs_1)) then
 				forward_a_1 <= "011";
 
 		elsif ((mem_wb_regWrite_2 = '1')
+			and (ex_mem_registerRd_2 /= "0000")
 			and not(ex_mem_regWrite_2 = '1'
 				and (ex_mem_registerRd_2 = id_ex_registerRs_1))
 			and (mem_wb_registerRd_2 = id_ex_registerRs_1)) then
@@ -64,20 +68,24 @@ begin
 		
 		-- second operand in first instruction
 		if ((ex_mem_regWrite_1 = '1') -- EX HAZARD
+			and (ex_mem_registerRd_1 /= "0000")
 			and (ex_mem_registerRd_1 = id_ex_registerRt_1)) then
 				forward_b_1 <= "001"; 
 
 		elsif((ex_mem_regWrite_2 = '1')
+			 and (ex_mem_registerRd_2 /= "0000")
 			 and (ex_mem_registerRd_2 = id_ex_registerRt_1)) then
 				forward_b_1 <= "010";
 				
 		elsif ((mem_wb_regWrite_1 = '1') -- MEM HAZARD
+			and (ex_mem_registerRd_1 /= "0000")
 			and not(ex_mem_regWrite_1 = '1' 
 				and (ex_mem_registerRd_1 = id_ex_registerRt_1))
 			and (mem_wb_registerRd_1 = id_ex_registerRt_1)) then
 				forward_b_1 <= "011";
 
 		elsif ((mem_wb_regWrite_2 = '1')
+			and (ex_mem_registerRd_2 /= "0000")
 			and not(ex_mem_regWrite_2 = '1'
 				and (ex_mem_registerRd_2 = id_ex_registerRt_1))
 			and (mem_wb_registerRd_2 = id_ex_registerRt_1)) then
@@ -88,20 +96,24 @@ begin
 
 		-- first operand in second instruction
 		if ((ex_mem_regWrite_1 = '1') -- EX HAZARD
+			and (ex_mem_registerRd_1 /= "0000")
 			and (ex_mem_registerRd_1 = id_ex_registerRs_2)) then
 				forward_a_2 <= "001"; 
 
 		elsif((ex_mem_regWrite_2 = '1')
+			and (ex_mem_registerRd_2 /= "0000")
 			 and (ex_mem_registerRd_2 = id_ex_registerRs_2)) then
 				forward_a_2 <= "010";
 				
 		elsif ((mem_wb_regWrite_1 = '1') -- MEM HAZARD
+			and (ex_mem_registerRd_1 /= "0000")
 			and not(ex_mem_regWrite_1 = '1' 
 				and (ex_mem_registerRd_1 = id_ex_registerRs_1))
 			and (mem_wb_registerRd_1 = id_ex_registerRs_2)) then
 				forward_a_2 <= "011";
 
 		elsif ((mem_wb_regWrite_2 = '1')
+			and (ex_mem_registerRd_2 /= "0000")
 			and not(ex_mem_regWrite_2 = '1'
 				and (ex_mem_registerRd_2 = id_ex_registerRs_2))
 			and (mem_wb_registerRd_2 = id_ex_registerRs_2)) then
@@ -112,10 +124,12 @@ begin
 		
 		-- second operand in second instruction
 		if ((ex_mem_regWrite_1 = '1') -- EX HAZARD
+			and (ex_mem_registerRd_1 /= "0000")
 			and (ex_mem_registerRd_1 = id_ex_registerRt_2)) then
 				forward_b_2 <= "001"; 
 
 		elsif((ex_mem_regWrite_2 = '1')
+			 and (ex_mem_registerRd_2 /= "0000")
 			 and (ex_mem_registerRd_2 = id_ex_registerRt_2)) then
 				forward_b_2 <= "010";
 				
